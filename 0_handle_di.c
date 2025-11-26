3c8fdd704f0053902ae0aec20029c2b8c620866c
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-26 15:44:22 +0100

    Change how the negatives is receives in 0_handle_di.c file

--- 0_handle_di.c
+++ 0_handle_di.c
@@ -11,38 +11,32 @@
 int handle_di(char array, va_list args)
 {
 	int nb = 0;
+	int value = va_arg(args, int);
+	char buffer[12];
+	int is_negative, j = 0, i = 0;
 
-	if (array == 'd' || array == 'i')
-	{
-		int value = va_arg(args, int);
-		char buffer[12];
-		int is_negative, j, i = 0;
-
-		if (value == 0)
-			return (0);
-
-		if (value < 0)
-		{
-			is_negative = 1;
-			value = -value;
-		}
-		while (value > 0)
-		{
-			buffer[i++] = '0' + (value % 10);
-			value /= 10;
-		}
-
-		if (is_negative)
-			buffer[i++] = '-';
-
-		for (j = i; j >= 0; j--)
-		{
-			write(1, &buffer[j], 1);
-			nb++;
-		}
-	}
-	else
+	if (value == 0)
 		return (0);
+
+	if (value < 0)
+	{
+		is_negative = 1;
+		value = - value;
+	}
+	while (value > 0)
+	{
+		buffer[i++] = '0' + (value % 10);
+		value /= 10;
+	}
+
+	if (is_negative == 1)
+		buffer[i++] = '-';
+
+	for (j = i; j >= 0; j--)
+	{
+		write(1, &buffer[j], 1);
+		nb++;
+	}
 
 	return (nb);
 }
