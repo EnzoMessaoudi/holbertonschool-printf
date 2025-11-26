96f2bcd61017173a18bb160a15f8aa23de54e53f
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-26 12:28:26 +0100

    Rename the file 0_handle_pourc.c in 0_handle_cs.c

--- 0_handle_cs.c
+++ 0_handle_cs.c
@@ -2,25 +2,27 @@
 #include <stdarg.h>
 
 /**
-* handle_pourc - Link to _printf and used to take in paramater the %
+* handle_cs% - Link to _printf and used to take in paramater the %
 * @array: Is used to change what the user want instead of the %
 * @args: Whqt the user want instead of the %
-* Return: Return false if not the good value and the number of characters if good
+* Return: Return false if not the good value or the number of characters
 */
 
-int handle_pourc(char array, va_list args)
+int handle_cs(char array, va_list args)
 {
 	int nb = 0;
 
 	if (array == 'c')
 	{
 		char c = va_arg(args, int);
+
 		write(1, &c, 1);
 		nb++;
 	}
 	else if (array == 's')
 	{
 		char *str = va_arg(args, char *);
+
 		while (*str)
 		{
 			write(1, str, 1);
@@ -32,7 +34,6 @@ int handle_pourc(char array, va_list args)
 	{
 		write(1, "%", 1);
 		nb++;
-		return (nb);
 	}
 	else
 		return (0);
