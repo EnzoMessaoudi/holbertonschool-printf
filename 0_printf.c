7d8a689aeb7ac29e4ab0e9c5b7bfa6e596a93ca1
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-27 07:21:36 +0100

    Change what the function handle_di takes as a parameter

--- 0_printf.c
+++ 0_printf.c
@@ -15,7 +15,7 @@ int _printf(const char *format, ...)
 			if (format[i + 1] == 'c' || format [i + 1] == 's' || format[i + 1] == '%')
 				return_pourc = handle_cs(format[i + 1], args);
 			else if (format[i + 1] == 'd' || format [i + 1] == 'i')
-				return_pourc = handle_di(format[i + 1], args);
+				return_pourc = handle_di(args);
 			else
 			{
 				write(1, &format[i], 1);
